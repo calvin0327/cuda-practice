bcb819b9dc64afab396f6f616c259c0669e7004f
calvin chen <120380290@qq.com>
2025-10-23 15:47:24 +0000

    update torch bindings
    Signed-off-by: calvin chen <120380290@qq.com>

--- csrc/core/registration.h
+++ csrc/core/registration.h
@@ -17,24 +17,6 @@
 #define TORCH_LIBRARY_IMPL_EXPAND(NAME, DEVICE, MODULE) \
   TORCH_LIBRARY_IMPL(NAME, DEVICE, MODULE)
 
-// extern "C" {
-// /* Creates a dummy empty _C module that can be imported from Python.
-//    The import from Python will load the .so consisting of this file
-//    in this extension, so that the TORCH_LIBRARY static initializers
-//    below are run. */
-// PyObject* PyInit__C(void) {
-//   static struct PyModuleDef module_def = {
-//       PyModuleDef_HEAD_INIT,
-//       "_C", /* name of module */
-//       NULL, /* module documentation, may be NULL */
-//       -1,   /* size of per-interpreter state of the module,
-//               or -1 if the module keeps state in global variables. */
-//       NULL, /* methods */
-//   };
-//   return PyModule_Create(&module_def);
-// }
-// }
-
 // REGISTER_EXTENSION allows the shared library to be loaded and initialized
 // via python's import statement.
 #define REGISTER_EXTENSION(NAME)                                               \
--- csrc/torch_bindings.cpp
+++ csrc/torch_bindings.cpp
@@ -5,16 +5,6 @@
 #include <torch/library.h>
 #include <torch/version.h>
 
-// // Defines the operators
-// TORCH_LIBRARY(cuda_kernel, m) {
-//   m.def("sgemm_naive_f32(Tensor a, Tensor b, Tensor c) -> ()");
-// }
-
-// // Registers CUDA implementations for mymuladd, mymul, myadd_out
-// TORCH_LIBRARY_IMPL(cuda_kernel, CUDA, m) {
-//   m.impl("sgemm_naive_f32", &sgemm_naive_f32);
-// }
-
 TORCH_LIBRARY_EXPAND(TORCH_EXTENSION_NAME, ops) {
   ops.def("sgemm_naive_f32(Tensor a, Tensor b, Tensor c) -> ()");
   ops.impl("sgemm_naive_f32", torch::kCUDA, &sgemm_naive_f32);
